a6571cc830dacaef950cab01923b54a70324511c
pallavig702 <31351096+pallavig702@users.noreply.github.com>
2024-09-30 11:46:32 -0500

    Create gpu_job.sh

---
+++ SLURM/gpu_job.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=palg_gpu_job
+#SBATCH --output=output_%j.txt
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=16G
+#SBATCH --time=06:00:00
+#SBATCH --partition=i.q # Get the name of the gpu_partition by the command "scontrol show partition"
+#SBATCH --gres=gpu:1              # Request 1 GPU
+
+module load cuda/10.2             # Load CUDA or other GPU-related modules
+
+source ~/LLaVaNV_env/bin/activate  #The environment equipped with all the dependentcies
+python RunLLaVa.py 
